d984e5578fed5bd688890e1a2ac8bb7ec00a5998
Kanishk Magare <Kanishkmagare55@gmail.com>
2023-09-11 09:08:28 +0530

    Add files via upload

---
+++ src/com/school/dto/Teacher.java
@@ -0,0 +1,41 @@
+package com.school.dto;
+
+public class Teacher {
+
+	private int id;
+	private String name;
+	private String email;
+	private int sal;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getSal() {
+		return sal;
+	}
+
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+}
